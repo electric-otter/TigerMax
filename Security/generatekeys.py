61f3b8a836f269587e485da079c2d562f0c309fa
electric-otter <krw122081@gmail.com>
2025-01-02 04:09:05 +0000

    Create generatekeys.py

---
+++ Security/generatekeys.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def generate_key(length):
+    characters = string.ascii_letters + string.digits
+    key = ''.join(random.choice(characters) for i in range(length))
+    return key
+
+def save_keys(filename, num_keys, key_length):
+    with open(filename, 'w') as file:
+        for _ in range(num_keys):
+            key = generate_key(key_length)
+            file.write(key + '\n')
+
+# Example usage
+save_keys('keys.txt', 100, 16)
