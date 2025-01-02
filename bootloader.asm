470bcb17270f539e9d7059aded6f56412705197a
electric-otter <krw122081@gmail.com>
2025-01-02 04:21:12 +0000

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -7,4 +7,24 @@ mov es,ax
 mov fs,ax
 mov gs,ax
 mov ss,ax
-jmp 8:10000h
+
+section .text
+   global _start         ;must be declared for using gcc
+    
+_start:                  ;tell linker entry point
+   ; Open the existing file for reading
+   mov  eax, 5           ; sys_open system call number
+   mov  ebx, file_name   ; file name
+   mov  ecx, 0           ; access mode (O_RDONLY)
+   int  0x80             ; call kernel
+   mov  [fd_out], eax
+
+   ; Keep the program running without exiting
+hang:
+   jmp hang
+
+section .data
+file_name db 'osselector.txt', 0
+
+section .bss
+fd_out resb 1
